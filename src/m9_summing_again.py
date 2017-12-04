61ec36a40a7b04e728bad94af5c566b56cb9e5cb
Allison Shi <shiae@rose-hulman.edu>
2017-12-03 23:18:14 -0500

    m9 DONE: 4 & 5

--- src/m9_summing_again.py
+++ src/m9_summing_again.py
@@ -78,7 +78,7 @@ def sum_powers(n, p):
 def run_test_sum_powers_in_range():
     """ Tests the   sum_powers_in_range   function. """
     # ------------------------------------------------------------------
-    # TODO: 4. Implement this function.
+    # DONE: 4. Implement this function.
     #   It TESTS the  sum_powers_in_range  function defined below.
     #   Include at least **   3   ** tests.
     #
@@ -121,13 +121,20 @@ def sum_powers_in_range(m, n, p):
       -- sum_powers_in_range(3, 100, 0.1) returns about 142.384776
     """
     # ------------------------------------------------------------------
-    # TODO: 5. Implement and test this function.
+    # DONE: 5. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     #
     #   No fair running the code of  sum_powers_in_range  to GENERATE
     #   test cases; that would defeat the purpose of TESTING!
     # ------------------------------------------------------------------
 
+    total = 0
+
+    for k in range(n + 1 - m):
+        add = (m + k) ** p
+        total = total + add
+
+    return total
 
 # ----------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
